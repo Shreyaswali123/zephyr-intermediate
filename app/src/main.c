5ef6020c6291ddbc136e9bd2047c01c6524b9783
shreyaswali123 <walishreyas55@gmail.com>
2026-08-16 11:20:19 +0530

    feat: demonstrate shared counter race condition

--- app/src/main.c
+++ app/src/main.c
@@ -1,70 +1,52 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(demo, LOG_LEVEL_INF);
 
 #define STACK_SIZE 1024
+#define PRIO_A 5
+#define PRIO_B 5
 
-#define PRIO_LOW  7
-#define PRIO_MED  5
-#define PRIO_HIGH 3
-#define PRIO_COOP -1
+#define ITERATIONS 1000
 
-void t_low_fn(void *p1, void *p2, void *p3)
+volatile int shared_counter = 0;
+
+void thread_a_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
-        LOG_INF("T_LOW running");
-        k_msleep(300);
-    }
-}
-
-void t_med_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_MED running");
-        k_msleep(200);
-    }
-}
-
-void t_high_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_HIGH running");
-        k_msleep(100);
-    }
-}
-
-void t_coop_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        for (int i = 0; i < 5; i++) {
-            volatile unsigned int work = 0;
-
-            for (unsigned int j = 0; j < 100000; j++) {
-                work += j;
-            }
-
-            LOG_INF("T_COOP busy iteration %d", i + 1);
-        }
-
+    for (int i = 0; i < ITERATIONS; i++) {
+        int temp = shared_counter;
         k_yield();
+        shared_counter = temp + 1;
     }
+
+    LOG_INF("Thread A finished");
 }
 
-K_THREAD_DEFINE(t_low, STACK_SIZE, t_low_fn,
-                NULL, NULL, NULL, PRIO_LOW, 0, 0);
+void thread_b_fn(void *p1, void *p2, void *p3)
+{
+    for (int i = 0; i < ITERATIONS; i++) {
+        int temp = shared_counter;
+        k_yield();
+        shared_counter = temp + 1;
+    }
 
-K_THREAD_DEFINE(t_med, STACK_SIZE, t_med_fn,
-                NULL, NULL, NULL, PRIO_MED, 0, 0);
+    LOG_INF("Thread B finished");
+}
 
-K_THREAD_DEFINE(t_high, STACK_SIZE, t_high_fn,
-                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
+K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
+                NULL, NULL, NULL, PRIO_A, 0, 0);
 
-K_THREAD_DEFINE(t_coop, STACK_SIZE, t_coop_fn,
-                NULL, NULL, NULL, PRIO_COOP, 0, 0);
+K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
+                NULL, NULL, NULL, PRIO_B, 0, 0);
 
 int main(void)
 {
-    LOG_INF("Thread scheduling demo started");
+    LOG_INF("Starting race condition demo");
+
+    k_msleep(1000);
+
+    LOG_INF("Final counter = %d, expected = %d",
+            shared_counter, ITERATIONS * 2);
+
     return 0;
 }
